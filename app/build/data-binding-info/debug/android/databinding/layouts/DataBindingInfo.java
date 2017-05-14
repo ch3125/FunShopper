66ba25025fb898647b8a61458418985aa2934ea2
Home <chhavi.alok25@gmail.com>
2017-05-14 09:10:13 +0530

    libraries

--- app/build/data-binding-info/debug/android/databinding/layouts/DataBindingInfo.java
+++ app/build/data-binding-info/debug/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="de009461-74e7-4401-b05f-ff18a7182777", modulePackage="com.ateam.funshoppers", sdkRoot="F:\\Android\\android-sdk-windows\\android-sdk-windows",layoutInfoDir="F:\\applications\\MyLemonade\\FunShopper\\app\\build\\layout-info\\debug",exportClassListTo="",isLibrary=false,minSdk=23,enableDebugLogs=false,printEncodedError=true)
+@BindingBuildInfo(buildId="50782dc8-d083-47f6-b630-6dc5024dc0b4", modulePackage="com.ateam.funshoppers", sdkRoot="F:\\Android\\android-sdk-windows\\android-sdk-windows",layoutInfoDir="F:\\applications\\MyLemonade\\FunShopper\\app\\build\\layout-info\\debug",exportClassListTo="",isLibrary=false,minSdk=23,enableDebugLogs=false,printEncodedError=true)
 public class DataBindingInfo {}
